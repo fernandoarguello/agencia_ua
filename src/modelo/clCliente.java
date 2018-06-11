a5cb576f681ff79a53ccd8dbd9c596e92574b520
Eduardo Giménez <Eduardo Gimnez@192.168.0.35>
2018-06-11 18:59:47 -0400

    Merge origin/master cliente constructores

---
+++ src/modelo/clCliente.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Eduardo Giménez
+ */
+public class clCliente {
+    private int idCliente;
+    private String documento;
+    private String TipodDocumento;
+    private String nombres;
+    private String apellidos;
+    private Date FechaNacimiento;
+    private String direccion;
+    private int idPais;
+    private int Ciudad;
+    private String telefono;
+    private String mail;
+    
+    public int getIdCliente() {
+        return idCliente;
+    }
+    
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getTipodDocumento() {
+        return TipodDocumento;
+    }
+
+    public void setTipodDocumento(String TipodDocumento) {
+        this.TipodDocumento = TipodDocumento;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public Date getFechaNacimiento() {
+        return FechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date FechaNacimiento) {
+        this.FechaNacimiento = FechaNacimiento;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getIdPais() {
+        return idPais;
+    }
+
+    public void setIdPais(int idPais) {
+        this.idPais = idPais;
+    }
+
+    public int getCiudad() {
+        return Ciudad;
+    }
+
+    public void setCiudad(int Ciudad) {
+        this.Ciudad = Ciudad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    
+   
+}
