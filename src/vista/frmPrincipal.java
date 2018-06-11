d5ddcbe2151c066e0f86f3987b7c2157d13f61ad
Eduardo Giménez <Eduardo Gimnez@192.168.0.35>
2018-06-11 17:48:21 -0400

    Merge origin/master VARIOS

--- src/vista/frmPrincipal.java
+++ src/vista/frmPrincipal.java
@@ -29,11 +29,15 @@ public class frmPrincipal extends javax.swing.JFrame {
 
         mnuPrincipal = new javax.swing.JMenuBar();
         mnuMenu = new javax.swing.JMenu();
-        mnuCambioContrasenha = new javax.swing.JMenuItem();
-        mnuSalir = new javax.swing.JMenuItem();
+        mnuitmCambioContrasenha = new javax.swing.JMenuItem();
+        mnuitmSalir = new javax.swing.JMenuItem();
         mnuMantenimiento = new javax.swing.JMenu();
+        mnuitmCliente = new javax.swing.JMenuItem();
+        mnuitmPais = new javax.swing.JMenuItem();
+        mnuitmCiudad = new javax.swing.JMenuItem();
         mnuGestion = new javax.swing.JMenu();
         mnuConsultas = new javax.swing.JMenu();
+        mnuAdministrar = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Menú Principal");
@@ -41,15 +45,36 @@ public class frmPrincipal extends javax.swing.JFrame {
 
         mnuMenu.setText("Menú");
 
-        mnuCambioContrasenha.setText("Cambiar Contraseña");
-        mnuMenu.add(mnuCambioContrasenha);
+        mnuitmCambioContrasenha.setText("Cambiar Contraseña");
+        mnuMenu.add(mnuitmCambioContrasenha);
 
-        mnuSalir.setText("Salir");
-        mnuMenu.add(mnuSalir);
+        mnuitmSalir.setText("Salir");
+        mnuitmSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mnuitmSalirActionPerformed(evt);
+            }
+        });
+        mnuMenu.add(mnuitmSalir);
 
         mnuPrincipal.add(mnuMenu);
 
         mnuMantenimiento.setText("Mantenimiento");
+
+        mnuitmCliente.setText("Cliente");
+        mnuitmCliente.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        mnuitmCliente.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+        mnuMantenimiento.add(mnuitmCliente);
+
+        mnuitmPais.setText("País");
+        mnuitmPais.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        mnuitmPais.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+        mnuMantenimiento.add(mnuitmPais);
+
+        mnuitmCiudad.setText("Ciudad");
+        mnuitmCiudad.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        mnuitmCiudad.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+        mnuMantenimiento.add(mnuitmCiudad);
+
         mnuPrincipal.add(mnuMantenimiento);
 
         mnuGestion.setText("Gestión");
@@ -57,6 +82,9 @@ public class frmPrincipal extends javax.swing.JFrame {
 
         mnuConsultas.setText("Consultas");
         mnuPrincipal.add(mnuConsultas);
+
+        mnuAdministrar.setText("Administrar");
+        mnuPrincipal.add(mnuAdministrar);
 
         setJMenuBar(mnuPrincipal);
 
@@ -73,6 +101,10 @@ public class frmPrincipal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void mnuitmSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnuitmSalirActionPerformed
+        dispose();
+    }//GEN-LAST:event_mnuitmSalirActionPerformed
 
     /**
      * @param args the command line arguments
@@ -110,12 +142,16 @@ public class frmPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenuItem mnuCambioContrasenha;
+    private javax.swing.JMenu mnuAdministrar;
     private javax.swing.JMenu mnuConsultas;
     private javax.swing.JMenu mnuGestion;
     private javax.swing.JMenu mnuMantenimiento;
     private javax.swing.JMenu mnuMenu;
     private javax.swing.JMenuBar mnuPrincipal;
-    private javax.swing.JMenuItem mnuSalir;
+    private javax.swing.JMenuItem mnuitmCambioContrasenha;
+    private javax.swing.JMenuItem mnuitmCiudad;
+    private javax.swing.JMenuItem mnuitmCliente;
+    private javax.swing.JMenuItem mnuitmPais;
+    private javax.swing.JMenuItem mnuitmSalir;
     // End of variables declaration//GEN-END:variables
 }
