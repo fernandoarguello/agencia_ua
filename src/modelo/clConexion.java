d0eb82784fb9b01867d5aa7ef42a89ffaaeebc76
claud <claud@192.168.0.30>
2018-06-11 19:17:15 -0400

    Merge origin/master
    Conflicts:
    	modelo_agencia.mwb
    	nbproject/project.properties

---
+++ src/modelo/clConexion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author agencia
+ */
+public class clConexion{
+    private final String base = "dbagencia";
+    private final String user = "root";
+    private final String password = "Inicial01";
+    private final String url = "jdbc:mysql://localhost:3306/" + base;
+    private Connection con = null;
+
+    public Connection getConexion() {
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = (Connection) DriverManager.getConnection(this.url, this.user, this.password);
+
+        } catch (SQLException e) {
+            System.err.println(e);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(clConexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
+}
