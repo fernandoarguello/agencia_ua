c3358233cafcdc493f4eef9e73065bcffc6ddbf8
claud <claud@172.16.8.61>
2018-06-14 20:22:57 -0400

    Merge origin/master
    Conflicts:
    	modelo_agencia.mwb
    	nbproject/project.properties

---
+++ src/modelo/ConsultaCiudad.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import com.mysql.jdbc.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author claud
+ */
+public class ConsultaCiudad extends clConexion{
+      public boolean registrar(clCiudad ciu) {
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+        String sql = "INSERT INTO Ciudad (idCiudad, idPais, descripcion) VALUES(?,?,?)";
+         try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, ciu.getIdPais());
+            ps.setString(3, ciu.getDescripcion());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+    
+
+ public boolean modificar(clCiudad ciu) {
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+
+        String sql = "UPDATE ciudad SET idCiudad=?, idPais=?, descripcion=?,WHERE id=? ";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, ciu.getIdCiudad());
+            ps.setInt(2, ciu.getIdPais());
+            ps.setString(3, ciu.getDescripcion());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+ public boolean eliminar(clCiudad ciu) {
+        PreparedStatement ps = null;
+        java.sql.Connection con = getConexion();
+
+        String sql = "DELETE FROM ciudad WHERE id=? ";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, ciu.getIdCiudad());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+ public boolean buscar(clCiudad ciu) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        java.sql.Connection con = getConexion();
+
+        String sql = "SELECT * FROM ciudad WHERE codigo=? ";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, ciu.getIdCiudad());
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                ciu.setIdCiudad(Integer.parseInt(rs.getString("ciudad")));
+                ciu.setIdPais(rs.getInt("pais"));
+                ciu.setDescripcion(rs.getString("descripcion"));
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+}
+
+    
+
---
+++ src/modelo/ConsultaPais.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import com.mysql.jdbc.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author claud
+ */
+public class ConsultaPais extends clConexion{
+    public boolean registrar(clPais pais) {
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+        String sql = "INSERT INTO Pais (idPais, idContinente, descripcion , nacionalidad) VALUES(?,?,?,?)";
+         try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, pais.getIdPais());
+            ps.setInt(2, pais.getIdContinente());
+            ps.setString(3, pais.getDescripcion());
+            ps.setString(4, pais.getNacionalidad());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+    
+
+ public boolean modificar(clPais pais) {
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+
+        String sql = "UPDATE pais SET idPais=?, idContinente=?, descripcion=?, nacionalidad=? WHERE id=? ";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, pais.getIdPais());
+            ps.setInt(2, pais.getIdContinente());
+            ps.setString(3, pais.getDescripcion());
+            ps.setString(4, pais.getNacionalidad());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+ public boolean eliminar(clPais pais) {
+        PreparedStatement ps = null;
+        java.sql.Connection con = getConexion();
+
+        String sql = "DELETE FROM pais WHERE id=? ";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, pais.getIdPais());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+ public boolean buscar(clPais pais) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        java.sql.Connection con = getConexion();
+
+        String sql = "SELECT * FROM pais WHERE codigo=? ";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, pais.getIdPais());
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                pais.setIdPais(Integer.parseInt(rs.getString("pais")));
+                pais.setIdContinente(rs.getInt("continente"));
+                pais.setDescripcion(rs.getString("descripcion"));
+                pais.setNacionalidad(rs.getString("nacionalidad"));
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+}
