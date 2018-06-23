291d58551b8ab386a18db0aeaf0dbc3390f31563
claud <claud@DESKTOP-FEB3BEJ>
2018-06-22 23:10:56 -0400

    Merge origin
    Conflicts:
    	modelo_agencia.mwb
    	nbproject/project.properties

--- src/modelo/ConsultaPaquete.java
+++ src/modelo/ConsultaPaquete.java
@@ -5,10 +5,113 @@
  */
 package modelo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author farguello
  */
 public class ConsultaPaquete {
-    
+    public boolean registrar (clPaquete paq) {
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+        
+        String sql = "INSERT INTO Paquete (idPaquete, idAtractivo, idCliente,FechaSalida,FechaRetorno) VALUES(?,?,?,?,?)";
+        
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, paq.getIdPaquete());
+            ps.setInt(2, paq.getIdAtractivo());
+            ps.setInt(3, paq.getIdCliente());
+            
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+    public boolean modificar(clContinente cont) {
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+
+        String sql = "UPDATE tblcontinente SET idContinente=?, descripcion=?";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cont.getIdContinente());
+            ps.setString(2, cont.getDescripcion());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+    public boolean eliminar(clContinente cont) {
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+
+        String sql = "DELETE FROM tblcontinente WHERE idContinente=? ";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cont.getIdContinente());
+            ps.execute();
+            return true;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
+    public boolean buscar(clContinente cont) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        Connection con = getConexion();
+
+        String sql = "SELECT * FROM tblcontinente WHERE idContinente=? ";
+
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setInt(1, cont.getIdContinente());
+            rs = ps.executeQuery();
+
+            if (rs.next()) {
+                cont.setIdContinente(Integer.parseInt(rs.getString("idContinente")));
+                cont.setDescripcion(rs.getString("descripcion"));
+                return true;
+            }
+            return false;
+        } catch (SQLException e) {
+            System.err.println(e);
+            return false;
+        } finally {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                System.err.println(e);
+            }
+        }
+    }
 }
