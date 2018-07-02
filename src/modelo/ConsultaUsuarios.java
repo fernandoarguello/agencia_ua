66695d9b32e93a665cee64b1f850024b5a080b4c
farguello <farguello@DESKTOP-TFV5NP8>
2018-07-01 21:02:28 -0400

    Se agrega la funcionalidad a la clase ConsultaUsuario

--- src/modelo/ConsultaUsuarios.java
+++ src/modelo/ConsultaUsuarios.java
@@ -9,6 +9,45 @@ package modelo;
  *
  * @author farguello
  */
-public class ConsultaUsuarios {
-    
+import com.mysql.jdbc.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class ConsultaUsuarios extends clConexion{
+    public boolean guardar(clUsuario usuario){
+        PreparedStatement ps = null;
+        com.mysql.jdbc.Connection con = getConexion();
+        String sql = "INSERT INTO dbagencia.tblusuario(IdUsuario, Nombre, Apellido, CorreoElectronico, Password) values(?,?,?,?,?);";
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1, usuario.getIdUsuario());
+            ps.setString(2, usuario.getNombre());
+            ps.setString(3, usuario.getApellido());
+            ps.setString(4, usuario.getCorreoElectronico());
+            
+            ps.setString(5, usuario.getPassword());
+            ps.execute();
+        }catch(SQLException e){
+            System.err.println(e);
+            return false;
+        }finally{
+            try{
+                con.close();
+            }catch(SQLException e){
+                System.err.println(e);
+            }
+        }
+        return true;
+    }
+    public boolean eliminar(){
+        return true;
+    }
+    public boolean modificar(){
+        return true;
+    }
+    public boolean buscar(){
+        return true;
+    }
 }
